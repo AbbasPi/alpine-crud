b343aaa4b5793645efbd0863a8cd33a9cc3c4655
Abbas Salah <abbas.log01@gmail.com>
2022-12-23 00:03:08 +0300

    fetch and render data from server using AlpineJS

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index, get_blogs
 
 urlpatterns = [
     path('', index, name='index'),
+    path('blogs/', get_blogs, name='blogs'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from blog.forms import BlogForm
+from blog.models import Blog
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    form = BlogForm()
+    blogs = Blog.objects.all()
+    ctx = {'form': form, 'blogs': blogs}
+    return render(request, 'blog/index.html', ctx)
+
+
+def get_blogs(request):
+    blogs = Blog.objects.all().values('title', 'description')
+    return JsonResponse({'blogs': list(blogs)}, safe=False)
